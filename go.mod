2e99ec2f0390e535e7db0202d97be0e5f59c6e5e
Michael Friese <mfriese@microsoft.com>
2025-10-22 03:14:12 +0200

    initial commit
    Signed-off-by: Michael Friese <mfriese@microsoft.com>

---
+++ go.mod
@@ -0,0 +1,77 @@
+module github.com/flyth/ig-extcap
+
+go 1.24.0
+
+require (
+	github.com/gopacket/gopacket v1.4.0
+	github.com/inspektor-gadget/inspektor-gadget v0.45.0
+)
+
+require (
+	github.com/blang/semver v3.5.1+incompatible // indirect
+	github.com/cilium/ebpf v0.19.1-0.20250729164112-d994daa25101 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/emicklei/go-restful/v3 v3.12.2 // indirect
+	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	github.com/fxamacker/cbor/v2 v2.9.0 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-openapi/jsonpointer v0.21.0 // indirect
+	github.com/go-openapi/jsonreference v0.21.0 // indirect
+	github.com/go-openapi/swag v0.23.1 // indirect
+	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/gnostic-models v0.7.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.4-0.20250319132907-e064f32e3674 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mailru/easyjson v0.9.0 // indirect
+	github.com/moby/moby v28.5.0+incompatible // indirect
+	github.com/moby/spdystream v0.5.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.3-0.20250322232337-35a7c28c31ee // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/mxk/go-flowrate v0.0.0-20140419014527-cca7078d478f // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.1 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sagikazarmark/locafero v0.11.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/sourcegraph/conc v0.3.1-0.20240121214520-5f936abd7ae8 // indirect
+	github.com/spf13/afero v1.15.0 // indirect
+	github.com/spf13/cast v1.10.0 // indirect
+	github.com/spf13/pflag v1.0.10 // indirect
+	github.com/spf13/viper v1.21.0 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	go.opentelemetry.io/otel v1.38.0 // indirect
+	go.opentelemetry.io/otel/metric v1.38.0 // indirect
+	go.yaml.in/yaml/v2 v2.4.2 // indirect
+	go.yaml.in/yaml/v3 v3.0.4 // indirect
+	golang.org/x/exp v0.0.0-20250408133849-7e4ce0ab07d0 // indirect
+	golang.org/x/net v0.44.0 // indirect
+	golang.org/x/oauth2 v0.30.0 // indirect
+	golang.org/x/sync v0.17.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
+	golang.org/x/term v0.35.0 // indirect
+	golang.org/x/text v0.29.0 // indirect
+	golang.org/x/time v0.13.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250825161204-c5933d9347a5 // indirect
+	google.golang.org/grpc v1.75.1 // indirect
+	google.golang.org/protobuf v1.36.10 // indirect
+	gopkg.in/evanphx/json-patch.v4 v4.12.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/api v0.34.1 // indirect
+	k8s.io/apimachinery v0.34.1 // indirect
+	k8s.io/client-go v0.34.1 // indirect
+	k8s.io/klog/v2 v2.130.1 // indirect
+	k8s.io/kube-openapi v0.0.0-20250710124328-f3f2b991d03b // indirect
+	k8s.io/utils v0.0.0-20250604170112-4c0f3b243397 // indirect
+	oras.land/oras-go/v2 v2.6.0 // indirect
+	sigs.k8s.io/json v0.0.0-20241014173422-cfa47c3a1cc8 // indirect
+	sigs.k8s.io/randfill v1.0.0 // indirect
+	sigs.k8s.io/structured-merge-diff/v6 v6.3.0 // indirect
+	sigs.k8s.io/yaml v1.6.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,318 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"net/url"
+	"os"
+	"runtime"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/gopacket/gopacket"
+	"github.com/gopacket/gopacket/layers"
+	"github.com/gopacket/gopacket/pcapgo"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/datasource"
+	gadgetcontext "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-context"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/logger"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators"
+	clioperator "github.com/inspektor-gadget/inspektor-gadget/pkg/operators/cli"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators/simple"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/params"
+	grpcruntime "github.com/inspektor-gadget/inspektor-gadget/pkg/runtime/grpc"
+)
+
+func main() {
+	var interfaceName string
+	flag.StringVar(&interfaceName, "interface", "ig", "interface name")
+
+	var packetFilter string
+	flag.StringVar(&packetFilter, "extcap-capture-filter", "not port 22", "packet filter")
+
+	var remoteAddress string
+	flag.StringVar(&remoteAddress, "remote-address", "tcp://10.211.55.12:1234", "remote address")
+
+	var fifo string
+	flag.StringVar(&fifo, "fifo", "", "fifo path")
+
+	var debug bool
+	flag.BoolVar(&debug, "debug", false, "set verbosity level to debug")
+
+	flag.BoolFunc("extcap-interfaces", "list extcap interfaces", func(s string) error {
+		fmt.Fprint(os.Stdout, "extcap {version=1.0.0}\n")
+		fmt.Fprint(os.Stdout, "interface {value=ig}{display=Inspektor Gadget (Daemon)}\n")
+		fmt.Fprint(os.Stdout, "interface {value=ig-k8s}{display=Inspektor Gadget on Kubernetes}\n")
+		os.Exit(0)
+		return nil
+	})
+
+	var showConfig bool
+	flag.BoolVar(&showConfig, "extcap-config", false, "show extcap configuration")
+
+	var extcapInterface string
+	flag.StringVar(&extcapInterface, "extcap-interface", "ig", "extcap interface")
+
+	var capture bool
+	flag.BoolVar(&capture, "capture", false, "start capture")
+
+	var gadgetImage string
+	flag.StringVar(&gadgetImage, "gadget-image", "tcpdump:latest", "gadget image")
+
+	var k8sNamespace string
+	var k8sPodname string
+	var k8sContainername string
+	var k8sSelector string
+	flag.StringVar(&k8sNamespace, "k8s-namespace", "", "k8s namespace")
+	flag.StringVar(&k8sPodname, "k8s-podname", "", "k8s podname")
+	flag.StringVar(&k8sContainername, "k8s-containername", "", "k8s container name")
+	flag.StringVar(&k8sSelector, "k8s-selector", "", "k8s selector")
+
+	var runtimeContainername string
+	flag.StringVar(&runtimeContainername, "runtime-containername", "", "runtime container name")
+
+	flag.Parse()
+
+	if showConfig {
+		switch interfaceName {
+		case "ig":
+			fmt.Fprint(os.Stdout, "arg {number=20}{call=--remote-address}{display=Remote Address}{type=string}{default=tcp://10.211.55.12:1234}{group=Remote}\n")
+			fallthrough
+		case "ig-k8s":
+			fmt.Fprint(os.Stdout, "arg {number=1}{call=--k8s-namespace}{display=Namespace}{type=string}{group=Kubernetes}\n")
+			fmt.Fprint(os.Stdout, "arg {number=2}{call=--k8s-podname}{display=Pod Name}{type=string}{group=Kubernetes}\n")
+			fmt.Fprint(os.Stdout, "arg {number=3}{call=--k8s-containername}{display=Container Name}{type=string}{group=Kubernetes}\n")
+			fmt.Fprint(os.Stdout, "arg {number=4}{call=--k8s-selector}{display=Label Selector}{type=string}{group=Kubernetes}\n")
+			fmt.Fprint(os.Stdout, "arg {number=5}{call=--runtime-containername}{display=Runtime Container Name}{type=string}{group=Containers}\n")
+		}
+		fmt.Fprint(os.Stdout, "arg {number=30}{call=--gadget-image}{display=Gadget Image}{type=string}{default=tcpdump:latest}{group=Gadget}\n")
+		//		fmt.Fprint(os.Stdout, "arg {number=1}{call=--snaplen}{display=Snaplen}{type=string}{default=tcpdump:latest}{group=Gadget}\n")
+		os.Exit(0)
+	}
+
+	if capture {
+		if interfaceName == "" {
+			os.Exit(0)
+		}
+
+		var f io.WriteCloser
+		if fifo != "" {
+			var err error
+			f, err = os.OpenFile(fifo, os.O_APPEND|os.O_WRONLY, 0600)
+			if err != nil {
+				os.Exit(1)
+			}
+			defer f.Close()
+		} else {
+			f = os.Stdout
+		}
+
+		// PCAP-NG writer
+		writer := simple.New("pcap-writer", simple.OnPreStart(func(gadgetCtx operators.GadgetContext) error {
+			datasources := gadgetCtx.GetDataSources()
+			ds, ok := datasources["packets"]
+			if !ok {
+				return errors.New("no packet datasource found")
+			}
+
+			// Check ds for compatiblity
+			payloadField := ds.GetField(ds.Annotations()[clioperator.AnnotationPCAPPayload])
+			if payloadField == nil {
+				return fmt.Errorf("%s annotation not found", clioperator.AnnotationPCAPPayload)
+			}
+			timestampField := ds.GetField(ds.Annotations()[clioperator.AnnotationPCAPTimestamp])
+			if timestampField == nil {
+				return fmt.Errorf("timestamp field not found")
+			}
+			lengthField := ds.GetField(ds.Annotations()[clioperator.AnnotationPCAPPacketLen])
+			if lengthField == nil {
+				return fmt.Errorf("length field not found")
+			}
+
+			// TODO:
+			// Currently, each data packet carries all interface/enriched information with the payload;
+			// optimally, we would create a special datasource for it and cache the sent information both
+			// on sender and receiver to save bandwidth.
+
+			// Find fields to be used for the interface key
+			var interfaceKeyFields []datasource.FieldAccessor
+			for _, f := range ds.Fields() {
+				if v, ok := f.Annotations[clioperator.AnnotationPCAPInterfaceKey]; ok && v == "true" {
+					acc := ds.GetField(f.FullName)
+					if acc == nil {
+						return fmt.Errorf("field %q not found", f.FullName)
+					}
+					interfaceKeyFields = append(interfaceKeyFields, acc)
+				}
+			}
+
+			// Find fields to be used for the interface variables; these variables will become
+			// part of the interface description and can be decoded using scripts in WireShark, for
+			// example. TODO: also add this functionality on a packet level.
+			var interfaceVarFns []func(datasource.Data, *strings.Builder)
+			for _, f := range ds.Fields() {
+				if v, ok := f.Annotations[clioperator.AnnotationPCAPInterfaceVar]; ok {
+					acc := ds.GetField(f.FullName)
+					if acc == nil {
+						return fmt.Errorf("field %q not found", f.FullName)
+					}
+					if acc.Type() != api.Kind_String && acc.Type() != api.Kind_CString {
+						return fmt.Errorf("field %q cannot be used as interface variable; currently, only string fields are supported", f.FullName)
+					}
+					interfaceVarFns = append(interfaceVarFns, func(data datasource.Data, sb *strings.Builder) {
+						str, err := acc.String(data)
+						if err != nil {
+							return
+						}
+						sb.WriteString(v)
+						sb.WriteByte('=')
+						sb.WriteString(url.QueryEscape(str))
+					})
+					interfaceKeyFields = append(interfaceKeyFields, acc)
+				}
+			}
+
+			var mu sync.Mutex
+
+			wr, err := pcapgo.NewNgWriter(f, layers.LinkTypeEthernet)
+			if err != nil {
+				return fmt.Errorf("creating pcapgo.NgWriter: %w", err)
+			}
+
+			interfaces := make(map[string]int)
+
+			ds.Subscribe(func(ds datasource.DataSource, data datasource.Data) error {
+				mu.Lock()
+				defer mu.Unlock()
+
+				var key strings.Builder
+				for _, f := range interfaceKeyFields {
+					key.Write(f.Get(data))
+					key.WriteByte(':') // Delimiter
+				}
+				ifIndex := key.String()
+
+				ts, _ := timestampField.Uint64(data)
+				payload, _ := payloadField.Bytes(data)
+				length, _ := lengthField.Uint32(data)
+
+				pcapInterface, ok := interfaces[ifIndex]
+				if !ok {
+					// Build description for interface
+					var description strings.Builder
+					for i, f := range interfaceVarFns {
+						if i > 0 {
+							description.WriteByte(';')
+						}
+						f(data, &description)
+					}
+
+					newInterface := pcapgo.NgInterface{
+						Name:        "virtual", // TODO: get from host
+						Description: description.String(),
+						OS:          runtime.GOOS, // TODO: get from host
+						LinkType:    layers.LinkTypeEthernet,
+					}
+					newIndex, err := wr.AddInterface(newInterface)
+					if err != nil {
+						return err
+					}
+					interfaces[ifIndex] = newIndex
+					pcapInterface = newIndex
+				}
+
+				err = wr.WritePacket(gopacket.CaptureInfo{
+					Timestamp:      time.Unix(0, int64(time.Duration(ts)*time.Microsecond)),
+					InterfaceIndex: pcapInterface,
+					CaptureLength:  len(payload),
+					Length:         int(length),
+				}, payload)
+				if err != nil {
+					gadgetCtx.Logger().Warnf("failed to write packet: %v", err)
+					return nil
+				}
+
+				// need to make sure this gets written before returning buffers
+				wr.Flush()
+				return nil
+			}, 10000)
+			return nil
+		}))
+
+		var runtime *grpcruntime.Runtime
+		var globalParams *params.Params
+		var rtParams *params.Params
+
+		pv := api.ParamValues{
+			"operator.oci.ebpf.pf":      packetFilter,
+			"operator.oci.ebpf.snaplen": "65535",
+		}
+
+		switch interfaceName {
+		default:
+			fmt.Println("unsupported extcap interface")
+			os.Exit(1)
+		case "ig":
+			runtime = grpcruntime.New()
+			globalParams = runtime.GlobalParamDescs().ToParams()
+			globalParams.Set("remote-address", remoteAddress)
+			rtParams = runtime.ParamDescs().ToParams()
+			if k8sNamespace != "" {
+				pv["operator.LocalManageer.k8s-namespace"] = k8sNamespace
+			}
+			if k8sContainername != "" {
+				pv["operator.LocalManageer.k8s-containername"] = k8sContainername
+			}
+			if k8sPodname != "" {
+				pv["operator.LocalManageer.k8s-podname"] = k8sPodname
+			}
+			if k8sSelector != "" {
+				pv["operator.LocalManageer.k8s-selector"] = k8sSelector
+			}
+			if runtimeContainername != "" {
+				pv["operator.LocalManageer.runtime-containername"] = runtimeContainername
+			}
+		case "ig-k8s":
+			runtime = grpcruntime.New(grpcruntime.WithConnectUsingK8SProxy)
+			globalParams = runtime.GlobalParamDescs().ToParams()
+			rtParams = runtime.ParamDescs().ToParams()
+			if k8sNamespace != "" {
+				pv["operator.KubeManager.k8s-namespace"] = k8sNamespace
+			}
+			if k8sContainername != "" {
+				pv["operator.KubeManager.k8s-containername"] = k8sContainername
+			}
+			if k8sPodname != "" {
+				pv["operator.KubeManager.k8s-podname"] = k8sPodname
+			}
+			if k8sSelector != "" {
+				pv["operator.KubeManager.k8s-selector"] = k8sSelector
+			}
+			if runtimeContainername != "" {
+				pv["operator.KubeManager.runtime-containername"] = runtimeContainername
+			}
+		}
+
+		err := runtime.Init(globalParams)
+		if err != nil {
+			panic(err)
+		}
+
+		// Run tcpdump gadget
+		l := logger.DefaultLogger()
+		l.SetLevel(logger.FatalLevel)
+		if debug {
+			l.SetLevel(logger.DebugLevel)
+		}
+
+		gadgetCtx := gadgetcontext.New(context.Background(), gadgetImage, gadgetcontext.WithLogger(l), gadgetcontext.WithDataOperators(writer))
+		err = runtime.RunGadget(gadgetCtx, rtParams, pv)
+		if err != nil {
+			os.Stderr.Write([]byte(err.Error()))
+		}
+	}
+}
